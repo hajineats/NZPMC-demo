b4e6c1a10547b11b74d540ef8981c0779c6f749c
hajin <68993476+hkim532@users.noreply.github.com>
2021-01-11 22:45:46 +1300

    [Stable ver] admin can add question with randomly generated options

--- src/admin-component/Admin.js
+++ src/admin-component/Admin.js
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import MathJax from "react-mathjax-preview";
 import firebase from "../firebase-component/Firebase";
 require("firebase/database");
+function makeOption(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 function Admin() {
   const [qNum, setQNum] = useState(null);
   const [latex, setLatex] = useState(String.raw``);
 
-  function handleChange(e) {
-    e.preventDefault();
-    setLatex(String.raw`${e.target.value}`);
-  }
-  function handleQNumChange(e) {
-    e.preventDefault();
-    setQNum(e.target.value);
-  }
   function submitQuestion(e) {
     e.preventDefault();
     firebase
@@ -23,10 +25,11 @@ function Admin() {
       .set({
         latex: latex,
         options: {
-          1: "Hajin",
-          2: "jaemin",
-          3: "anvilly",
-          4: "metal",
+          1: makeOption(12),
+          2: makeOption(10),
+          3: makeOption(11),
+          4: makeOption(10),
+          5: makeOption(10),
         },
       });
   }
@@ -34,14 +37,25 @@ function Admin() {
     <div>
       <label>Enter latex : </label>
       <br />
-      Question Number: <input type="textfield" onChange={handleQNumChange} />
+      Question Number:{" "}
+      <input
+        type="textfield"
+        onChange={(e) => {
+          e.preventDefault();
+          setQNum(e.target.value);
+        }}
+      />
       <br />
       <textarea
         style={{ minHeight: "200px", minWidth: "200px" }}
-        onChange={handleChange}
+        onChange={(e) => {
+          e.preventDefault();
+          setLatex(String.raw`${e.target.value}`);
+        }}
       />
-      <MathJax math={latex} />
       <button onClick={submitQuestion}>submit this question</button>
+      <br />
+      <MathJax math={latex} />
     </div>
   );
 }
