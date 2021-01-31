10ffcb7931c468e9f4ce75dc1b7b538e78b0756c
hajin <68993476+hkim532@users.noreply.github.com>
2021-01-31 13:21:35 +1300

    [feat] added timer feature (mock atm)
    need to include database query at the start

--- src/main-component/component/currentquestion/QuestionPage.js
+++ src/main-component/component/currentquestion/QuestionPage.js
@@ -8,6 +8,7 @@ import Button from "@material-ui/core/Button";
 import { Typography } from "@material-ui/core";
 
 export default function QuestionPage() {
+  const [timer, setTimer] = useState("");
   const [navClosed, setNavClosed] = useState(true);
   const [q, setQ] = useState(null);
   const [qIndex, setQIndex] = useState(-1);
@@ -22,6 +23,26 @@ export default function QuestionPage() {
     { checked: false, index: 8, prevChoice: -1 },
     { checked: false, index: 9, prevChoice: -1 },
   ]);
+
+  setInterval(() => {
+    var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (seconds.length === 1) {
+      seconds = "0" + seconds;
+    }
+    if (minutes.length === 1) {
+      minutes = "0" + minutes;
+    }
+    let timeLeft = hours + ":" + minutes + ":" + seconds;
+    setTimer(timeLeft);
+  }, 1000);
 
   return (
     <>
@@ -47,10 +68,14 @@ export default function QuestionPage() {
       </div>
       <Grid container item xs={12} spacing={3}>
         <Grid item xs={9}>
-          <Button onClick={() => setNavClosed(false)}>
-            <MenuIcon fontSize="large" />
-            <Typography>See remaining Questions/submit</Typography>
-          </Button>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Button onClick={() => setNavClosed(false)}>
+              <MenuIcon fontSize="large" />
+              <Typography>View Questions/submit</Typography>
+            </Button>
+            <Typography>Remaining Time: {timer}</Typography>
+          </div>
+
           <Question q={q} />
         </Grid>
         <Grid item xs={3}>
